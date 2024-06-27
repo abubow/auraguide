ae36a91243447f26a15bad5dfc895cfa10e79f20
Charsi420 <abdul.wahib51@gmail.com>
2024-06-27 18:24:25 +0500

    Implemented Paddle OCR

--- src/ocr/ocr.py
+++ src/ocr/ocr.py
@@ -1,21 +1,27 @@
 # src/ocr/ocr.py
 
-import time
 import cv2
+from paddleocr import PaddleOCR, draw_ocr
 
+ocr = PaddleOCR(det_model_dir=None, rec_model_dir='PaddleOCR/ocr_rec/en_PP-OCRv3_rec', use_angle_cls=True, lang='en')
 def perform_ocr(cap, lock, stop_event):
-    window_name = "OCR"
-    cv2.namedWindow(window_name)
-    while not stop_event.is_set():
-        with lock:
-            ret, image = cap.read()
-            if not ret:
-                break
-        if image is not None:
-            cv2.imshow(window_name, image)
+    while True:
+        ret, frame = cap.read()
+        cv2.imshow('OCR Video Feed', frame)
+
+            # If frame is read correctly, ret is True
+        if not ret:
+            print("Error: Can't receive frame (stream end?). Exiting ...")
+
+        # Perform OCR on the frame
+        result = ocr.ocr(frame, cls=True)
+
+        # Extract and print OCR text
+        if len(result) != 0:
+            for line in result:
+                if line is not None:
+                    for word_info in line:
+                        word = word_info[1][0]
+                        print(f"OCR Text: {word}")
         if cv2.waitKey(1) & 0xFF == ord('q'):
-            stop_event.set()
             break
-        time.sleep(0.1)  # Simulate some processing time
-    cv2.destroyWindow(window_name)
-    print("OCR Stopped.")
